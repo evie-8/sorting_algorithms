4026ef49d05c4c7d37c6b7ac0a541c133d58ac35
evie-8 <neouma29@gmail.com>
2023-06-24 21:33:54 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,38 @@
 #include "sort.h"
 /**
-* bubble_sort - sorts array using bubble bubble_sort
-* @array: araay to be sorted
+* bubble_sort - sorts array using selection sort
+* @array: array to be sorted
 * @size: size of array
 */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
 
+	if (size < 2 || !array)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1 ; j++)
+		{
+		if (array[j] > array[j + 1])
+		{
+			swapper(&array[j], &array[j + 1]);
+			print_array(array, size);
+		}
+		}
+	}
 }
 
+/**
+* swapper - swaps two numbers
+* @a: first num
+* @b: secon num
+*/
+void swapper(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
