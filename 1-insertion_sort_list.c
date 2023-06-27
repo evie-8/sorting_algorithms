69442153f0dd2eb93b5cb5cf937f41a626b9bc2f
evie-8 <neouma29@gmail.com>
2023-06-28 01:35:00 +0300

    corrections for insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sorts list or array using insertion sort
+ * insertion_sort_list - sorts list using insertion sort
  * @list: list to be sorted
 */
 void insertion_sort_list(listint_t **list)
